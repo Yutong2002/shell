a44457aa2fb96b421808b3870c7e3c23c055d29c
Yutong Wang <142604405+Yutong2002@users.noreply.github.com>
2025-10-07 22:41:26 -0400

    Update assignment.sh

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -46,12 +46,7 @@ cp raw/*server*.log processed/server_logs/
 cp raw/*user*.log processed/user_logs/
 cp raw/*event*.log processed/event_logs/
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-<<<<<<< HEAD
 rm -f raw/*ipaddr* processed/user_logs/*ipaddr*
-=======
-rf -rf ./data
-
->>>>>>> ea20676d33161a6f4d0fcd3c4f7aa5360f0f4309
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 touch inventory.txt
 ls -R processed > inventory.txt
